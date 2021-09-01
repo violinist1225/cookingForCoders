ad33bc31a3ecd6008f059890ec7fb3788c4b6937
root <root@DESKTOP-Q835I3D.localdomain>
2021-09-01 10:21:23 -0700

    changes-in-server

--- server.js
+++ server.js
@@ -40,11 +40,6 @@ mongoose.connect(
 
   // if (process.env.NODE_ENV === 'production' ) { app.use(express.static('client/build)) }
 
-
-// app.get("*", (req, res) => {
-//     res.sendFile(path.join, (__dirname, "client", "build", "index.html"))
-// })
-
 // Accessing the path module
 const path = require("path");
 
